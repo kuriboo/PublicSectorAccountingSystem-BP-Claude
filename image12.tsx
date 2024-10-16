06f9f65e8c6ff02880c058b5a4d60d7c9011100b
kuriboo <kuriboo5@gmail.com>
2024-10-17 01:29:46 +0900

    Add or update image12.tsx

--- image12.tsx
+++ image12.tsx
@@ -1,153 +1,213 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// 予約情報を表すインターフェース
-interface Reservation {
-  date: string;
-  time: string;
-  adultCount: number;
-  childCount: number;
-  infantCount: number;
-  seatClass: string;
-  smoking: boolean;
-}
+// 型定義
+type ReserveFormProps = {
+  fromPref?: string;
+  fromCity?: string;
+  fromStation?: string;
+  toPref?: string;
+  toCity?: string; 
+  toStation?: string;
+  date?: string;
+  time?: string;
+  trainType?: string;
+  ticketType?: string;
+  adultNum?: number;
+  childNum?: number;
+  onSubmit: () => void;
+};
 
-// コンポーネントのプロパティを定義するインターフェース
-interface ReservationFormProps {
-  onSubmit: (reservation: Reservation) => void;
-}
-
-// スタイル付きコンポーネントを定義
+// スタイル定義
 const FormWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 20px;
   background-color: #f0f0f0;
-  border-radius: 5px;
-`;
 
-const FormRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 10px;
-  width: 100%;
-  
-  @media (max-width: 600px) {
-    flex-direction: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-around;
   }
 `;
 
-const FormLabel = styled.label`
-  margin-right: 10px;
+const FieldSet = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+  }
 `;
 
-const FormInput = styled.input`
-  padding: 5px;
-  border-radius: 3px;
-  border: 1px solid #ccc;
+const Legend = styled.legend`
+  font-weight: bold;
 `;
 
-const FormSelect = styled.select`
+const Label = styled.label`
+  margin-bottom: 5px;
+`;
+
+const Select = styled.select`
   padding: 5px;
-  border-radius: 3px;
-  border: 1px solid #ccc;
+  font-size: 16px;
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  font-size: 16px;
 `;
 
 const SubmitButton = styled.button`
   padding: 10px 20px;
+  font-size: 18px;
   background-color: #007bff;
-  color: #fff;
+  color: white;
   border: none;
   border-radius: 5px;
   cursor: pointer;
-  
+
   &:hover {
     background-color: #0056b3;
   }
 `;
 
-// 予約フォームコンポーネント
-const ReservationForm: React.FC<ReservationFormProps> = ({ onSubmit }) => {
-  // フォームの状態を管理
-  const [reservation, setReservation] = React.useState<Reservation>({
-    date: '',
-    time: '',
-    adultCount: 0,
-    childCount: 0,
-    infantCount: 0,
-    seatClass: '',
-    smoking: false,
-  });
-
-  // フォームの入力が変更されたときの処理
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value, type, checked } = e.target;
-    const inputValue = type === 'checkbox' ? checked : value;
-    setReservation(prevState => ({
-      ...prevState,
-      [name]: inputValue,
-    }));
-  };
-
-  // フォームが送信されたときの処理
-  const handleSubmit = (e: React.FormEvent) => {
+// コンポーネント実装
+const ReserveForm: React.FC<ReserveFormProps> = ({
+  fromPref = '',
+  fromCity = '',
+  fromStation = '',
+  toPref = '',
+  toCity = '',
+  toStation = '',
+  date = '',
+  time = '',
+  trainType = '指定なし',
+  ticketType = '',
+  adultNum = 1,
+  childNum = 0,
+  onSubmit,
+}) => {
+  // フォーム送信処理
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmit(reservation);
+    onSubmit();
   };
 
   return (
-    <FormWrapper>
-      <form onSubmit={handleSubmit}>
-        <FormRow>
-          <FormLabel>予約日:</FormLabel>
-          <FormInput type="date" name="date" value={reservation.date} onChange={handleChange} required />
-        </FormRow>
-        <FormRow>
-          <FormLabel>予約時間:</FormLabel>
-          <FormInput type="time" name="time" value={reservation.time} onChange={handleChange} required />
-        </FormRow>
-        <FormRow>
-          <FormLabel>大人:</FormLabel>
-          <FormInput type="number" name="adultCount" value={reservation.adultCount} onChange={handleChange} min={0} required />
-        </FormRow>
-        <FormRow>
-          <FormLabel>子供:</FormLabel>
-          <FormInput type="number" name="childCount" value={reservation.childCount} onChange={handleChange} min={0} required />
-        </FormRow>
-        <FormRow>
-          <FormLabel>幼児:</FormLabel>
-          <FormInput type="number" name="infantCount" value={reservation.infantCount} onChange={handleChange} min={0} required />
-        </FormRow>
-        <FormRow>
-          <FormLabel>座席クラス:</FormLabel>
-          <FormSelect name="seatClass" value={reservation.seatClass} onChange={handleChange} required>
-            <option value="">選択してください</option>
-            <option value="economy">エコノミー</option>
-            <option value="business">ビジネス</option>
-          </FormSelect>
-        </FormRow>
-        <FormRow>
-          <FormLabel>喫煙席:</FormLabel>
-          <FormInput type="checkbox" name="smoking" checked={reservation.smoking} onChange={handleChange} />
-        </FormRow>
-        <SubmitButton type="submit">予約する</SubmitButton>
-      </form>
-    </FormWrapper>
+    <form onSubmit={handleSubmit}>
+      <FormWrapper>
+        <FieldSet>
+          <Legend>出発地</Legend>
+          <Label>
+            都道府県
+            <Select value={fromPref}>
+              <option value="">選択してください</option>
+              {/* 都道府県のオプション */}
+            </Select>
+          </Label>
+          <Label>
+            市区町村
+            <Input type="text" value={fromCity} />
+          </Label>
+          <Label>
+            駅名
+            <Input type="text" value={fromStation} />
+          </Label>
+        </FieldSet>
+
+        <FieldSet>
+          <Legend>到着地</Legend>
+          <Label>
+            都道府県
+            <Select value={toPref}>
+              <option value="">選択してください</option>
+              {/* 都道府県のオプション */}
+            </Select>
+          </Label>
+          <Label>
+            市区町村  
+            <Input type="text" value={toCity} />
+          </Label>
+          <Label>
+            駅名
+            <Input type="text" value={toStation} />
+          </Label>
+        </FieldSet>
+
+        <FieldSet>
+          <Legend>乗車日</Legend>
+          <Label>
+            日付
+            <Input type="date" value={date} />
+          </Label>
+          <Label>
+            時刻
+            <Input type="time" value={time} />
+          </Label>
+        </FieldSet>
+
+        <FieldSet>
+          <Legend>詳細条件</Legend>
+          <Label>
+            列車種別
+            <Select value={trainType}>
+              <option value="指定なし">指定なし</option>
+              {/* 列車種別のオプション */}
+            </Select>
+          </Label>
+          <Label>
+            座席タイプ
+            <Select value={ticketType}>
+              <option value="">選択してください</option>
+              <option value="自由席">自由席</option>
+              <option value="指定席">指定席</option>
+            </Select>
+          </Label>
+          <Label>
+            大人
+            <Input type="number" value={adultNum} min={1} />
+          </Label>
+          <Label>
+            子供
+            <Input type="number" value={childNum} min={0} />  
+          </Label>
+        </FieldSet>
+      </FormWrapper>
+
+      <SubmitButton type="submit">検索</SubmitButton>
+    </form>
   );
 };
 
-// 使用例
+// サンプルデータ
+const sampleData = {
+  fromPref: '東京都',
+  fromCity: '新宿区',
+  fromStation: '新宿',
+  toPref: '大阪府',
+  toCity: '大阪市',  
+  toStation: '新大阪',
+  date: '2023-05-01',
+  time: '10:00',
+  trainType: '新幹線',
+  ticketType: '指定席',
+  adultNum: 2,
+  childNum: 1,
+};
+
+// 表示用コンポーネント
 const App: React.FC = () => {
-  const handleReservationSubmit = (reservation: Reservation) => {
-    // フォームの送信処理を行う
-    console.log(reservation);
+  const handleSubmit = () => {
+    console.log('検索ボタンがクリックされました');
   };
 
   return (
     <div>
-      <h1>予約フォーム</h1>
-      <ReservationForm onSubmit={handleReservationSubmit} />
+      <h1>新幹線予約フォーム</h1>
+      <ReserveForm {...sampleData} onSubmit={handleSubmit} />
     </div>
   );
 };
